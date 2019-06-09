72a9c4cb94713e090e97511719c732f8ed6ecc6d
smolangu <smolangu@SMOLANGU-IN.in.oracle.com>
2019-06-09 11:23:56 +0530

    Added initial react client and server code

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -5,9 +5,45 @@
 // - socket.io-client
 
 const os = require('os');
+const io = require('socket.io-client');
+let socket = io('http://127.0.0.1:8181');
+socket.on('connect', () => {
+    const nI = os.networkInterfaces();
+    let macA;
+    // loop through all the nI for this machine and find a non-internal one
+    for(let key in nI){
+        if(!nI[key][0].internal){
+                macA = nI[key][0].mac;
+                break;
+        }
+    }
+    // client auth with single key value
+    socket.emit('clientAuth', '5t78yuhgirekjaht32i3')
 
-function performanceData(){
-    return new Promise(async (resolve, reject)=>{
+    
+    performanceData().then((allPerformanceData)=>{
+        allPerformanceData.macA = macA
+        socket.emit('initPerfData',allPerformanceData)
+    });
+
+    // start sending over data on interval
+    let perfDataInterval = setInterval(() => {
+        performanceData().then((allPerformanceData) => {
+            // console.log(allPerformanceData)
+            allPerformanceData.macA = macA;
+            socket.emit('perfData', allPerformanceData);
+        })
+    }, 1000);
+
+    socket.on('disconnect',()=>{
+        clearInterval(perfDataInterval);
+    })
+
+
+})
+
+function performanceData() {
+    return new Promise(async (resolve, reject) => {
         const cpus = os.cpus();
         // What do we need to know from node about performance?
         // - CPU load (current)
@@ -17,7 +53,7 @@ function performanceData(){
         //  - total
         const totalMem = os.totalmem();
         const usedMem = totalMem - freeMem;
-        const memUseage = Math.floor(usedMem/totalMem*100)/100;
+        const memUseage = Math.floor(usedMem / totalMem * 100) / 100;
         // - OS type
         const osType = os.type() == 'Darwin' ? 'Mac' : os.type();
         // - uptime
@@ -30,22 +66,22 @@ function performanceData(){
         //  - Clock Speed
         const cpuSpeed = cpus[0].speed
         const cpuLoad = await getCpuLoad();
-        resolve({freeMem,totalMem,usedMem,memUseage,osType,upTime,cpuModel,numCores,cpuSpeed,cpuLoad})
+        resolve({ freeMem, totalMem, usedMem, memUseage, osType, upTime, cpuModel, numCores, cpuSpeed, cpuLoad })
     })
 }
 
 // cpus is all cores. we need the average of all the cores which
 // will give us the cpu average
-function cpuAverage(){
+function cpuAverage() {
     const cpus = os.cpus();
     // get ms in each mode, BUT this number is since reboot
     // so get it now, and get it in 100ms and compare
     let idleMs = 0;
     let totalMs = 0;
     // loop through each core
-    cpus.forEach((aCore)=>{
+    cpus.forEach((aCore) => {
         // loop through each property of the current core
-        for(type in aCore.times){
+        for (type in aCore.times) {
             totalMs += aCore.times[type];
         }
         idleMs += aCore.times.idle;
@@ -59,10 +95,10 @@ function cpuAverage(){
 // because the times property is time since boot, we will get
 // now times, and 100ms from now times. Compare them, that will
 // give us current Load
-function getCpuLoad(){
-    return new Promise((resolve, reject)=>{
+function getCpuLoad() {
+    return new Promise((resolve, reject) => {
         const start = cpuAverage();
-        setTimeout(()=>{
+        setTimeout(() => {
             const end = cpuAverage();
             const idleDifference = end.idle - start.idle;
             const totalDifference = end.total - start.total;
@@ -70,11 +106,7 @@ function getCpuLoad(){
             // calc the % of used cpu
             const percentageCpu = 100 - Math.floor(100 * idleDifference / totalDifference);
             resolve(percentageCpu);
-        },100)
+        }, 100)
     })
 }
-
-performanceData().then((allPerformanceData)=>{
-    console.log(allPerformanceData)
-})
 
